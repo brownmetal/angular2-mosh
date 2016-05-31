87f266ceb7575e93ccec8b320aaa6c62a40c9731
sunil <sun.aec@gmail.com>
2016-06-01 01:16:36 +0530

    Exercise-Adding Author Component+Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import { CoursesComponent } from './courses.component';
+import { AuthorComponent } from './author.component';
 @Component({
     selector: 'my-app',
-    template: '<h1>Welcome to Angular2</h1><courses></courses>',
-    directives: [CoursesComponent]
+    template: '<h1>Welcome to Angular2</h1><courses></courses><author></author>',
+    directives: [CoursesComponent, AuthorComponent]
 })
 export class AppComponent { }
