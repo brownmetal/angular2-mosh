ffc5a185138d54d50f3d50e8722b6b6b98ab1ec2
sunil <sun.aec@gmail.com>
2016-06-01 01:23:00 +0530

    Exercise: author service

--- app/author.component.js
+++ app/author.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', './author.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,24 +10,28 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, author_service_1;
     var AuthorComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (author_service_1_1) {
+                author_service_1 = author_service_1_1;
             }],
         execute: function() {
             AuthorComponent = (function () {
-                function AuthorComponent() {
-                    this.authors = ['Author1', 'Auhtor2', 'Author2'];
+                function AuthorComponent(authorService) {
+                    this.authors = authorService.getAuthors();
                 }
                 AuthorComponent = __decorate([
                     core_1.Component({
                         selector: 'author',
-                        template: "<h2>Authors</h2>\n               <ul>\n                    <li *ngFor=\"let author of authors\">{{author}}</li>\n               </ul>\n    "
+                        template: "<h2>Authors</h2>\n               <ul>\n                    <li *ngFor=\"let author of authors\">{{author}}</li>\n               </ul>\n    ",
+                        providers: [author_service_1.AuthorService]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [author_service_1.AuthorService])
                 ], AuthorComponent);
                 return AuthorComponent;
             }());
--- app/author.component.ts
+++ app/author.component.ts
@@ -1,16 +1,20 @@
 import { Component} from '@angular/core';
-
+import { AuthorService } from './author.service';
 @Component({
     selector: 'author',
     template: `<h2>Authors</h2>
                <ul>
                     <li *ngFor="let author of authors">{{author}}</li>
                </ul>
-    `
+    `,
+    providers: [AuthorService]
 })
 export class AuthorComponent {
-    authors:string[]=['Author1', 'Auhtor2', 'Author2'];
-    constructor() { }
+    //authors:string[]=['Author1', 'Auhtor2', 'Author2'];
+    authors;
+    constructor(authorService: AuthorService) {
+        this.authors=authorService.getAuthors();
+     }
 
     
 
