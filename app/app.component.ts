2370ea41c15278fbfcc96f87cf8185c0ec9ea2ea
sunil <sun.aec@gmail.com>
2016-06-01 23:53:30 +0530

    Class Binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,11 @@ import { CoursesComponent } from './courses.component';
 import { AuthorComponent } from './author.component';
 @Component({
     selector: 'my-app',
-    template: '<h1>Welcome to Angular2</h1><courses></courses><author></author>',
+    template: `<h1>Welcome to Angular2</h1><courses></courses><author></author>
+                <button class="btn btn-primary" [class.active]="isActive">Submit</button>
+    `,
     directives: [CoursesComponent, AuthorComponent]
 })
-export class AppComponent { }
+export class AppComponent { 
+    isActive=false;
+}
